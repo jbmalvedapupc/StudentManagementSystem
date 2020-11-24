aa2de946017e775ef2faea2523fd600c2e996df3
JBMalveda <jbmalveda.pupc@gmail.com>
2020-11-24 13:06:36 +0800

    Added Student Create page.

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master_layout')
+
+@section('content')
+    {{-- Page Heading --}}
+    <h1 class="h3 mb-4 text-gray-800">New Student</h1>
+    {{-- /Page Heading --}}
+    
+    
+    <div class="row">
+        
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'DashboardController@index')->name('dashboard.index');
+// Student Module
+Route::get('/students/create', 'StudentController@create')->name('student.create');
