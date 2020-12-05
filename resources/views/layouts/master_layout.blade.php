7d855cf1ca21ddff0db049d1929c590fcc39ed14
JBMalveda <jbmalveda.pupc@gmail.com>
2020-12-05 09:42:47 +0800

    Added Datatables to Search Page

--- resources/views/layouts/master_layout.blade.php
+++ resources/views/layouts/master_layout.blade.php
@@ -10,16 +10,14 @@
     <meta name="author" content="">
 
     <title>{{ config('app.name') }}</title>
-
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
+    @yield('page_level_css')
 </head>
 
 <body id="page-top">
@@ -92,6 +90,7 @@
     <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    @yield('page_level_scripts')
 </body>
 
 </html>
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master_layout')
 
+@section('page_level_css')
+    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
     {{-- Page Heading --}}
     <h1 class="h3 mb-4 text-gray-800">Search Student</h1>
@@ -7,6 +11,57 @@
     
     
     <div class="row">
-        
+        <div class="col-12">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="student-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>Middle Name</th>
+                            <th>Section</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>3</td>
+                            <td>Tamad</td>
+                            <td>Juan</td>
+                            <td>Xavier</td>
+                            <td>Section 01</td>
+                            <td>
+                                <a class="btn btn-sm btn-primary" href="#">View</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Magic</td>
+                            <td>Mina</td>
+                            <td>Gertrud</td>
+                            <td>Section 02</td>
+                            <td>
+                                <a class="btn btn-sm btn-primary" href="#">View</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
+@endsection
+
+@section('page_level_scripts')
+    <!-- Page level plugins -->
+    <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script>
+        // Call the dataTables jQuery plugin
+        $(document).ready(function() {
+            $('#student-table').DataTable();
+        });
+    </script>
 @endsection
